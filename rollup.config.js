3e0bfdb5cac9b5f9d75e38f66d31dca082d452b6
Chad Watson <cwatson@dmp.com>
2019-03-22 13:12:52 -0500

    Removes react from build

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import babel from "rollup-plugin-babel";
 import { uglify } from "rollup-plugin-uglify";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 export default {
   input: "src/index.js",
@@ -8,6 +9,7 @@ export default {
     format: "cjs"
   },
   plugins: [
+    peerDepsExternal(),
     babel({
       exclude: "node_modules/**",
       babelrc: false,
