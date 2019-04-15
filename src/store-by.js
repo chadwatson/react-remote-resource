e08934c1a5fb900f5ba23386bbf7a8cf68242219
Chad Watson <chadwatsn@gmail.com>
2019-04-15 17:31:22 -0500

    Fixes storeBy API

--- src/store-by.js
+++ src/store-by.js
@@ -1,12 +1,15 @@
+import { curry } from "ramda";
 import withLens from "./with-lens";
 
-const storeBy = createKey =>
+const storeBy = curry((createKey, load) =>
   withLens(
     (currentState = {}) => (...args) => currentState[createKey(...args)],
     (currentState = {}) => (...args) => data => ({
       ...currentState,
       [createKey(...args)]: data
-    })
-  );
+    }),
+    load
+  )
+);
 
 export default storeBy;
