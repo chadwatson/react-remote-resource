6958c050e486f10c36b7b31467a73638df99de87
Seth MacPherson <smacpherson64@gmail.com>
2019-04-23 12:43:41 -0500

    test: removes legacy mocks and mock params

--- src/__mocks__/create-mock-resource.js
+++ src/__mocks__/create-mock-resource.js
@@ -1,20 +1,11 @@
 import createResource from "../create-resource";
-import createSingleEntryResource from "../create-single-entry-resource";
 
-export const createMockResource = (
-  getter,
-  setter,
-  entryPredicate,
-  loader,
-  getInitialValue
-) => {
+export const createMockResource = (getter, setter, entryPredicate, loader) => {
   const spies = {
     getter: jest.fn().mockImplementation(getter),
     setter: jest.fn().mockImplementation(setter),
     entryPredicate: jest.fn().mockImplementation(entryPredicate),
-    loader: jest.fn().mockImplementation(loader),
-    getInitialValue:
-      getInitialValue && jest.fn().mockImplementation(getInitialValue)
+    loader: jest.fn().mockImplementation(loader)
   };
 
   return [
@@ -22,22 +13,8 @@ export const createMockResource = (
       spies.getter,
       spies.setter,
       spies.entryPredicate,
-      spies.loader,
-      spies.getInitialValue
+      spies.loader
     ),
-    spies
-  ];
-};
-
-export const createMockSingleEntryResource = (loader, getInitialValue) => {
-  const spies = {
-    loader: jest.fn().mockImplementation(loader),
-    getInitialValue:
-      getInitialValue && jest.fn().mockImplementation(getInitialValue)
-  };
-
-  return [
-    createSingleEntryResource(spies.loader, spies.getInitialValue),
     spies
   ];
 };
